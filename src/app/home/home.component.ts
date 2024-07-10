e60e6f1836e96fdc2cac34054306f17365dc4246
teran724 <id2.sistemas@724.com.mx>
2024-07-10 11:52:53 -0600

    pagina

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import GLightbox from 'glightbox';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  constructor() {}
+
+  ngAfterViewInit() {
+    const lightbox = GLightbox({
+      selector: 'glightbox'
+    });
+  }
 
 }
